81ac09c75d86138b293cd12f92c55c060713ff9f
prajwalmw <prajwalwaingankar@yahoo.in>
2019-09-15 16:47:02 +0530

    FirebaseMessagingService added and manifest default noti icon added

---
+++ app/src/main/java/com/nivala/akshyata/foodsanta/services/MyFirebaseMessagingService.java
@@ -0,0 +1,14 @@
+package com.nivala.akshyata.foodsanta.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+
+}
