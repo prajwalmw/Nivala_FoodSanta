24c8d1e6da4edda5123cee6f4f0a303f96e568d1
prajwalmw <prajwalwaingankar@yahoo.in>
2019-09-15 17:12:27 +0530

    notification logic done

--- app/src/main/java/com/nivala/akshyata/foodsanta/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/nivala/akshyata/foodsanta/services/MyFirebaseMessagingService.java
@@ -1,14 +1,67 @@
 package com.nivala.akshyata.foodsanta.services;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.nivala.akshyata.foodsanta.R;
+import com.nivala.akshyata.foodsanta.Take_Act;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = MyFirebaseMessagingService.class.getName();
+
     public MyFirebaseMessagingService() {
     }
 
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //super.onMessageReceived(remoteMessage);
 
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+
+            sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        }
+
+
+
+    }
+
+    private void sendNotification(String title, String body) {
+        Intent intent = new Intent(this, Take_Act.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
 }
