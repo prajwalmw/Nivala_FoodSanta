dd8ad3371e644a2912da1e86f233abc49c8acac6
Unknown <prajwalwaingankar@yahoo.in>
2019-09-15 23:09:51 +0530

    index.js code logic added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendGiveDataAddedTrigger = functions.database.ref('/giver_data/{pushId}').onWrite(event =>
+	{
+		const give = event.data.val();
+		const payload = {
+			notification : {
+				title : 'New Food Added',
+				body : `${give.titleData}`
+			}
+		};
+
+	return admin.messaging().sendToTopic("giver_data",payload)
+		.then(response =>
+		{
+			return console.log('Notification sent successfully', response);
+		})
+		.catch(error =>
+		{
+			return console.log('Notification sent unsucessful', error);
+		});
+
+	});
+
+	
